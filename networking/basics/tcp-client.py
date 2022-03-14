0d3642f10498f8d6054d267b3bca6fbf0caf2dc9
whois-sin? <89893869+whois-sin@users.noreply.github.com>
2022-03-13 22:08:26 -0400

    Organized directories and added python networking basics section. Also added tcp-client script.

---
+++ networking/basics/tcp-client.py
@@ -0,0 +1,25 @@
+import socket
+
+# target_host accepts site as a standalone domain name
+target_host = "example.com" 
+target_port = 80
+
+# creates a socket object
+# AF_INET indicates IPv4 address or hostname
+# SOCK_STREAM indicates that this will be a TCP client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connects the client to the specified host and port
+client.connect((target_host,target_port))
+
+# client.send needs a byte like object
+# sends data as bytes by adding the "b" before the string
+# you can alternatively use (bytes("GET...", "UTF-8"))
+client.send(b"GET / HTTP/1.1\r\nHOST: example.com\r\n\r\n")
+
+# receives response from the server 
+response = client.recv(8192)
+
+# prints response from the server and decodes the bytes to UTF-8 format
+print(response.decode)
+client.close()
