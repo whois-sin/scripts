ad0c2fa380163a91d9ab7921110dd7d739fe6a97
whois-sin? <89893869+whois-sin@users.noreply.github.com>
2022-03-23 08:43:12 -0400

    Updated htb variables and python tcp client cleaning up the code.

--- htb/htb-workflow.sh
+++ htb/htb-workflow.sh
@@ -3,8 +3,12 @@
 # export environment variables for current session
 export HTB="devzat"; # Name of the machine
 export IP="10.129.216.120";  # Target IP 
-export URL="$HTB.htb"; # Target domain
-export FUZZ="$DOMAIN/FUZZ/"; # Fuzzing path
+export DOMAIN="$HTB.htb"; # Target domain
+export URL="http://$DOMAIN" # Target URL http
+export URLS="https://$DOMAIN" # Target URL https 
+export FUZZ="http://$DOMAIN/FUZZ/"; # Fuzzing http path
+export FUZZS="https://$DOMAIN/FUZZS/"; # Fuzzing https path
+export SUB="Host: FUZZ.$DOMAIN"; # Fuzzing for subdomains/vhosts
 
 # create baseline directories
 mkdir -p $HTB/{creds,enum,files,vulns}; 
@@ -17,8 +21,8 @@ mkdir -p vulns/{cves,payloads}
 touch creds/{hashes,creds.txt,passwords.txt,users.txt};
 
 # add host to /etc/hosts
-echo "$IP $URL has been added to our /etc/hosts file." | sudo tee -a /etc/hosts
+echo "$IP $DOMAIN has been added to our /etc/hosts file." | sudo tee -a /etc/hosts
 
 echo "[+] We're currently working on the $HTB box on Hack the Box."
-echo "[+] Our target IP is $IP, at the $URL domain."
+echo "[+] Our target IP is $IP, at the $DOMAIN domain."
 echo "[+] Use $FUZZ as the url for directory brute forcing with ffuf if there is a web-server on the machine."
--- networking/basics/tcp-client.py
+++ networking/basics/tcp-client.py
@@ -7,19 +7,17 @@ target_port = 80
 # creates a socket object
 # AF_INET indicates IPv4 address or hostname
 # SOCK_STREAM indicates that this will be a TCP client
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
 
 # connects the client to the specified host and port
-client.connect((target_host,target_port))
+    client.connect((target_host,target_port))
 
 # client.send needs a byte like object
 # sends data as bytes by adding the "b" before the string
 # you can alternatively use (bytes("GET...", "UTF-8"))
-client.send(b"GET / HTTP/1.1\r\nHOST: example.com\r\n\r\n")
-
-# receives response from the server 
-response = client.recv(8192)
-
+    client.send(b"GET / HTTP/1.1\r\nHOST: example.com\r\n\r\n")
+    print(client.send)
+# receives response from the server byte size is relative to content length
+    response = client.recv(8192)
 # prints response from the server and decodes the bytes to UTF-8 format
-print(response.decode)
-client.close()
+print(response.decode())
